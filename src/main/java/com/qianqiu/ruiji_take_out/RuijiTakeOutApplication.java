9f1b8fabe4db90f2d425a0b2e85504a6ecaf76e9
qianqiu <qianqiu@qq.com>
2023-06-01 21:46:12 +0800

    使用Sharding-JDBC实现读写分离，完成了数据库主从

--- src/main/java/com/qianqiu/ruiji_take_out/RuijiTakeOutApplication.java
+++ src/main/java/com/qianqiu/ruiji_take_out/RuijiTakeOutApplication.java
@@ -1,13 +1,16 @@
 package com.qianqiu.ruiji_take_out;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j
+//告诉spring我手动配置数据源
 @SpringBootApplication
 //开启过滤器
 @ServletComponentScan
